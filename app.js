fa2a22368450fb34302f7ad5fb7da4c7c4d1ceed
santidev <santi.chpy@gmail.com>
2022-07-21 23:25:19 +0700

    config webhook 1.16

--- app.js
+++ app.js
@@ -6,18 +6,28 @@ const logger = require('morgan');
 const dotenv = require('dotenv');
 dotenv.config();
 
+//cors
+const cors = require('cors');
+
+
 const indexRouter = require('./routes/index');
 const lineRouter =  require('./routes/line');
 
 const app = express();
 
+app.use(cors()); // allow all origin
 app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter); //  http://localhost:4000
 app.use('/line',lineRouter); // http://localhost:4000/line/callback
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+
+
 
 module.exports = app;
