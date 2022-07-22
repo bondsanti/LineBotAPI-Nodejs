5e99766859cdd39681e3c901d2f91a052a76c652
santidev <santi.chpy@gmail.com>
2022-07-23 01:40:11 +0700

    handle

--- routes/line.js
+++ routes/line.js
@@ -1,9 +1,19 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const config = require('../config/line');
+const service = require('../services/handle-event');
 
 // http://localhost:4000/..../callback
-router.post('/callback', function(req, res, next) {
-  res.send('Line Callback');
+router.post('/callback',config.line.middleware(config.lineConfig) ,(req, res, next) =>{
+  // res.send('Line Callback');
+  Promise
+    .all(req.body.events.map(service.handleEvent))
+    .then((result) => res.json(result))
+    .catch((err) => {
+      console.error(err);
+      res.status(500).end();
+    });
 });
+
 
 module.exports = router;
---
+++ services/handle-event.js
@@ -0,0 +1,46 @@
+const config = require('../config/line');
+const service = require('./handle-message');
+
+// event handler
+exports.handleEvent = (event) => {
+    //console.log(event);
+    // if (event.type !== 'message' || event.message.type !== 'text') {
+    //   // ignore non-text-message event
+    //   return Promise.resolve(null);
+    // }
+  
+    // // create a echoing text message
+    // const echo = { type: 'text', text: event.message.text };
+  
+    // // use reply API
+    // return config.client.replyMessage(event.replyToken, echo);
+
+    switch (event.type) {
+        case "message":
+            // console.log('message');
+            switch (event.message.type) {
+                case "text":
+                   // console.log('Text message');
+                   service.handleMessage(event);
+                case "sticker":
+                   // console.log('Sticker message');
+                    break;
+                case "image":
+                   // console.log('Image message');
+                default:
+                    throw new Error("Unknow message "+JSON.stringify(event.message.type));
+                   
+            }
+            break;
+        case "postback":
+            console.log('postback');
+            break;
+    
+        default:
+            throw new Error("Unknow event "+JSON.stringify(event));
+           
+    }
+  }
+
+  
+  
